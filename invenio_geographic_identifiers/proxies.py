7be14992d17ea1006c5a5372cdcccdc19d6dac4f
Felipe Menino <efelipecarlos@gmail.com>
2022-07-19 18:28:49 -0300

    service: add proxies

---
+++ invenio_geographic_identifiers/proxies.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 GEO Secretariat.
+#
+# invenio-geographic-identifiers is free software; you can redistribute it
+# and/or modify it under the terms of the MIT License; see LICENSE file for
+# more details.
+
+from flask import current_app
+from werkzeug.local import LocalProxy
+
+
+def _ext_proxy(attr):
+    """Get proxy from a specific extension attribute.
+
+    Note:
+        This code was adapted from Invenio Vocabularies.
+    """
+    return LocalProxy(
+        lambda: getattr(current_app.extensions["invenio-geographic-identifiers"], attr)
+    )
+
+
+current_geoidentifier_service = _ext_proxy("geoidentifiers_service")
+"""Proxy to the instantiated vocabulary service."""
+
+current_geoidentifier_resource = _ext_proxy("geoidentifiers_resource")
+"""Proxy to the instantiated vocabulary resource."""
