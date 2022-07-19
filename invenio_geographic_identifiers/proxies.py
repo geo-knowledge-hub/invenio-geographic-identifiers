0b0621e31979fe31953c6409a0e52e37caf89a78
Felipe Menino <efelipecarlos@gmail.com>
2022-07-19 19:37:15 -0300

    service: fixing docstring in the proxies file

--- invenio_geographic_identifiers/proxies.py
+++ invenio_geographic_identifiers/proxies.py
@@ -6,6 +6,8 @@
 # and/or modify it under the terms of the MIT License; see LICENSE file for
 # more details.
 
+"""Invenio Geographic Identifiers proxies."""
+
 from flask import current_app
 from werkzeug.local import LocalProxy
 
